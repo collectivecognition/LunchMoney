563fbc320174d3e89c96c8e907717b40da97cfe5
Will Blanchette <will@unata.com>
2014-12-06 11:34:56 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+	private CharacterController controller;
+	private Vector3 speed = new Vector3(3, 0, 5);
+	
+	void Start () {
+		// controller = GetComponent<CharacterController>();
+	}
+
+	void Update () {
+	}
+
+	void FixedUpdate () {
+		Vector3 direction = Vector3.zero;
+		direction = new Vector3 (Input.GetAxis ("Horizontal") * speed.x, 0, Input.GetAxis ("Vertical") * speed.z);
+		// transform.Translate(direction * Time.deltaTime);
+		rigidbody.velocity = direction;
+	}
+}
