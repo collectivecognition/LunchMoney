cd5863cdec3026b9395012a21d09e0bb38273250
Will Blanchette <will@unata.com>
2014-12-07 14:31:38 -0500

    Adding files

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
+	public Transform player;
+
 	private bool onElevator;
 	private Animator animator;
 	private float hp = 3;
+	private bool attacking = true;
+	private float speed = 1f;
 
 	void Start () {
 		animator = GetComponent<Animator> ();
@@ -13,9 +17,35 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(attacking){
+			comeAtMeBro();
+		}else{
+			wander();
+		}
 	}
 
+	void FixedUpdate () {
+
+		// Blink randomly
+		
+		if(Random.value < 0.02){
+			animator.SetTrigger("blink");
+		}
+	}
+
+	private void wander() {
+	}
+
+	private void comeAtMeBro() {
+		float distance = Vector3.Distance (transform.position, player.position);
+		if(distance > 2f){
+			transform.position = Vector3.MoveTowards (transform.position, player.position, speed * Time.deltaTime);
+			animator.SetBool("walking", true);
+		}else{
+			animator.SetBool ("walking", false);
+		}
+	}
+	
 	public void hit(float force){
 		animator.SetTrigger ("hit");
 		rigidbody.AddForce (new Vector3 (force / 2, Mathf.Abs(force), 0));
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -24,25 +24,25 @@ public class PlayerControls : MonoBehaviour {
 	}
 
 	void Update () {
+
+		// Animate picking up objects
+
 		if(pickingUp && Time.time > pickupStartTime + pickupDelay){
 			carrying.transform.position = Vector3.Lerp(pickupPosition, carryingPosition, (Time.time - pickupStartTime - pickupDelay) * pickupSpeed / pickupDistance);
-			// carrying.transform.rotation = Quaternion.Lerp(Quaternion.identity, carryingRotation, (Time.time - pickupStartTime - pickupDelay) * pickupSpeed / Quaternion.Angle(pickupRotation, carryingRotation));
-			carrying.transform.rotation = Quaternion.RotateTowards (carrying.transform.rotation, Quaternion.AngleAxis(90, Vector3.forward), pickupSpeed);
+			// carrying.transform.rotation = Quaternion.RotateTowards (carrying.transform.rotation, Quaternion.AngleAxis(90, Vector3.forward), pickupSpeed);
 			if(carrying.transform.position == carryingPosition){
 				pickingUp = false;
 			}
-			//private hit.transform.Rotate (Vector3.forward * 90, Space.World);
-
-			// carrying.transform.position = Vector3.Lerp (carrying.transform.position, carryingPosition, pickupSpeed * Time.deltaTime);
-			// hit.transform.position = new Vector3(transform.position.x, transform.position.y + 0.7f, transform.position.z - 0.1f);
 		}
 	}
 
 	void FixedUpdate () {
 			if(!pickingUp){
-				float direction = -Mathf.Sign(transform.localScale.x);
+				float direction = transform.localEulerAngles.y == 0 ? -1 : 1;
 				float up = Input.GetAxis ("Vertical") * speed.z;
-				// transform.Translate(direction * Time.deltaTime);
+	
+				// Apply movement velocity
+
 				rigidbody.velocity = new Vector3 (Input.GetAxis ("Horizontal") * speed.x, rigidbody.velocity.y, up);
 
 				if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){
@@ -54,11 +54,11 @@ public class PlayerControls : MonoBehaviour {
 				// Mirror animations
 
 				if(Input.GetAxis("Horizontal") > 0){
-					transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+					transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 180f, transform.localEulerAngles.z);
 				}
 
 				if(Input.GetAxis("Horizontal") < 0){
-					transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+					transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 0f, transform.localEulerAngles.z);
 				}
 
 				// Blink randomly
@@ -71,20 +71,19 @@ public class PlayerControls : MonoBehaviour {
 
 				if(Input.GetKeyDown(KeyCode.Space)){
 					animator.SetTrigger("punch");
-
-					Vector3 forward = transform.TransformDirection(Vector3.left) * -direction;
+					Vector3 forward = transform.TransformDirection(Vector3.left);
 					RaycastHit hit;
 					if(Physics.Raycast(transform.position, forward, out hit)){
 						if(hit.distance <= punchDistance){
-							// if(hit.transform.tag == "Enemy"){
-								// hit.transform.GetComponent<Enemy>().hit (punchForce * direction);
-							// }
 							hit.rigidbody.AddForce (new Vector3(punchForce.x * direction, punchForce.y, punchForce.z));
 						}
 					}
 				}
 
 				if(Input.GetKeyDown(KeyCode.RightShift)){
+
+					// Throw
+
 					if(carrying){
 						carrying.transform.parent = null;
 						carrying.rigidbody.detectCollisions = true;
@@ -92,36 +91,36 @@ public class PlayerControls : MonoBehaviour {
 						carrying.rigidbody.AddForce (new Vector3(throwSpeed.x * direction, throwSpeed.y, throwSpeed.z));
 						animator.SetTrigger("throw");
 						carrying = null;
+
+					// Pick up
+
 					}else{
-						Vector3 forward = transform.TransformDirection(Vector3.left) * -direction;
-						RaycastHit hit;
-						if(Physics.Raycast(transform.position, forward, out hit)){
-							if(hit.distance <= punchDistance){
-								if(hit.transform.tag == "Pickup"){
-									pickingUp = true;
-									
-									// Attach to player
-									
-									hit.transform.parent = transform;
-									
-									// Turn off physics
-									
-									hit.rigidbody.detectCollisions = false;
-									hit.rigidbody.isKinematic = true;
-									
-									// Set up some variables for the transition
-									
-									carrying = hit.transform;
-									carryingPosition = new Vector3(transform.position.x, transform.position.y + 0.7f, transform.position.z - 0.1f);
-									pickupDistance = Vector3.Distance (carryingPosition, carrying.position);
-									pickupStartTime = Time.time;
-									pickupPosition = carrying.position;
-									
-									// Start animation
-									
-									animator.SetTrigger("carry");
-								}
-							}
+						Transform closest = Util.FindClosestWithTag(transform, "Pickup", new Vector3(1f, 9999f, 2f));
+						Debug.Log (closest);
+						
+						if(closest){
+							pickingUp = true;
+							
+							// Attach to player
+							
+							closest.transform.parent = transform;
+							
+							// Turn off physics
+							
+							closest.rigidbody.detectCollisions = false;
+							closest.rigidbody.isKinematic = true;
+							
+							// Set up some variables for the transition
+							
+							carrying = closest.transform;
+							carryingPosition = new Vector3(transform.position.x, transform.position.y + 0.7f, transform.position.z);
+							pickupDistance = Vector3.Distance (carryingPosition, carrying.position);
+							pickupStartTime = Time.time;
+							pickupPosition = carrying.position;
+							
+							// Start animation
+							
+							animator.SetTrigger("carry");
 						}
 					}
 			}
